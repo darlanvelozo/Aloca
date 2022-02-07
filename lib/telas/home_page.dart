8b7cec0db3f9634ffadbc7da2e5985cf673eb24f
darlanvelozo <44929764+darlanvelozo@users.noreply.github.com>
2022-02-07 15:52:23 -0300

    0.3

--- lib/telas/home_page.dart
+++ lib/telas/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -10,6 +11,7 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
+    int _page = 0;
     var largura = MediaQuery.of(context).size.width;
     var altura = MediaQuery.of(context).size.height;
     return Scaffold(
@@ -19,16 +21,23 @@ class _HomePageState extends State<HomePage> {
         ),
         backgroundColor: Colors.greenAccent,
       ),
-      body: ListView(
-        children: [
-          Container(
-            color: Colors.grey.shade300,
-            child: Column(
-              children: [],
-            ),
-          ),
+      bottomNavigationBar: CurvedNavigationBar(
+        backgroundColor: Colors.white,
+        buttonBackgroundColor: Colors.white,
+        color: Colors.greenAccent,
+        index: _page,
+        items: const <Widget>[
+          Icon(Icons.list, size: 30),
+          Icon(Icons.add, size: 30),
+          Icon(Icons.info_outline, size: 30),
         ],
+        onTap: (index) {
+          setState(() {
+            _page = index;
+          });
+        },
       ),
+      body: Container(),
     );
   }
 }
